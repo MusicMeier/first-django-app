268e389341314cde3bed16242425b9f0f3a4c5b0
Music <musicm987@gmail.com>
2021-01-15 14:22:35 -0700

    hide secret key

--- firstDjangoProject/settings.py
+++ firstDjangoProject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-# from decouple import config
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY='!%zl1yahh35xcui$^(0(=s@t3y*qw*bvh2^m_mdqs)wxd0g4=9'
+SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
